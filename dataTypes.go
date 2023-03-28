e2e3ceb3e3e7fde8d89c737ef7ffa6be8191a3aa
Dicky Pratama <dickypratama4792@gmail.com>
2023-03-28 12:46:08 +0700

    leaarn data types

---
+++ dataTypes.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var wholeNumber uint8 = 34
+	var decimalNumber = 2.0
+	var varBool = true
+
+	fmt.Println(wholeNumber)
+	fmt.Println(decimalNumber)
+	fmt.Println(varBool)
+}
